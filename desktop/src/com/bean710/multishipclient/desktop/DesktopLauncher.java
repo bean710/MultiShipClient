dbff72ddf272063ee43806018e2a415c32e067a5
Ben Keener <keener4christ@gmail.com>
2016-08-14 16:44:10 -0400

    Initial commit

---
+++ desktop/src/com/bean710/multishipclient/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bean710.multishipclient.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bean710.multishipclient.MultiShip;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultiShip(), config);
+	}
+}
